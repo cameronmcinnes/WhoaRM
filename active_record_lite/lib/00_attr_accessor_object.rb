03724f5d061025cce8d4bea30cd39db96d638469
cameron mcinnes <crm2193@columbia.edu>
2018-02-02 12:22:50 -0500

    Start ActiveRecordLite, complete up to Ig, #insert

--- active_record_lite/lib/00_attr_accessor_object.rb
+++ active_record_lite/lib/00_attr_accessor_object.rb
@@ -1,5 +1,21 @@
+# require "byebug"
+
 class AttrAccessorObject
   def self.my_attr_accessor(*names)
-    
+    names.each do |ivar_name|
+
+      getter = "@#{ivar_name}"
+
+      define_method(ivar_name) do
+        self.instance_variable_get(getter)
+      end
+
+      setter = "#{ivar_name}="
+      ivar_name_w_at = "@#{ivar_name}"
+
+      define_method(setter.to_sym) do |val|
+        self.instance_variable_set(ivar_name_w_at, val)
+      end
+    end
   end
 end
--- active_record_lite/lib/01_sql_object.rb
+++ active_record_lite/lib/01_sql_object.rb
@@ -5,38 +5,83 @@ require 'active_support/inflector'
 
 class SQLObject
   def self.columns
-    # ...
+    return @columns if @columns
+
+    table_info = DBConnection.execute2(<<-SQL)
+      SELECT
+        *
+      FROM
+        #{table_name}
+    SQL
+
+    @columns = table_info.first.map(&:to_sym)
   end
 
   def self.finalize!
+    columns.each do |column_name|
+      define_method(column_name) do
+        self.attributes[column_name] # self is instance  of class (because we're inside define method)
+      end
+
+      setter = "#{column_name}="
+
+      define_method(setter.to_sym) do |val|
+        self.attributes[column_name] = val
+      end
+    end
   end
 
   def self.table_name=(table_name)
-    # ...
+    @table_name = table_name
   end
 
   def self.table_name
-    # ...
+    @table_name ||= self.to_s.tableize
   end
 
   def self.all
-    # ...
+    results = DBConnection.execute(<<-SQL)
+      SELECT
+        #{table_name}.*
+      FROM
+        #{table_name}
+    SQL
+
+    self.parse_all(results)
   end
 
   def self.parse_all(results)
-    # ...
+    results.map { |params| self.new(params) }
   end
 
   def self.find(id)
-    # ...
+    results = DBConnection.execute(<<-SQL, id)
+      SELECT
+        #{table_name}.*
+      FROM
+        #{table_name}
+      WHERE
+        #{table_name}.id = ?
+    SQL
+
+    return nil if results.empty?
+
+    self.parse_all(results).first
   end
 
   def initialize(params = {})
-    # ...
+    params.each do |attr_name, value|
+      attr_sym = attr_name.to_sym
+      unless self.class.columns.include?(attr_sym)
+        raise "unknown attribute '#{attr_name}'"
+      end
+
+      self.send("#{attr_sym}=", value)
+    end
   end
 
   def attributes
-    # ...
+    @attributes ||= {}
   end
 
   def attribute_values
